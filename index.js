935d564b6b728d4cb075a235b35339546061dfe0
anoopraju31 <anoopraju31@gmail.com>
2023-07-12 21:23:54 +0530

    adding new amount to sell

--- index.js
+++ index.js
@@ -12,4 +12,19 @@ const puppeteer = require('puppeteer')
 	// select chain
 	await page.type('#react-select-2-input', 'Arbitrum One')
 	await page.keyboard.press('Enter')
+
+	// adding amount to sell
+	await page.click(
+		'#__next > div > div > div.sc-889ee977-0.gCbopq > main > div.sc-55ee4011-1.cZHlms > div.sc-55ee4011-3.dlZmAt > div.sc-55ee4011-0.iXoIVV > div.css-1urcov8 > div:nth-child(1) > div.css-1k491an > div > input',
+	)
+
+	// deleting already present value in the input (10)
+	await page.keyboard.press('Backspace')
+	await page.keyboard.press('Backspace')
+
+	// adding new value
+	await page.type(
+		'#__next > div > div > div.sc-889ee977-0.gCbopq > main > div.sc-55ee4011-1.cZHlms > div.sc-55ee4011-3.dlZmAt > div.sc-55ee4011-0.iXoIVV > div.css-1urcov8 > div:nth-child(1) > div.css-1k491an > div > input',
+		'12',
+	)
 })()
